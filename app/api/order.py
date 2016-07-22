756fbfcff787605ab9995eec11a38ab44482c811
wrestle <wsxinchina@gmail.com>
2016-07-22 03:46:53 -0700

    Write user.py Still Not Complete it Because of the Email Function Just Write Done without Test Check it all

--- app/api/order.py
+++ app/api/order.py
@@ -4,8 +4,3 @@ from . import api
 from flask import request,jsonify
 from ..models import User,Order
 
-@api.route('/listorders/<int:userid>', methods=['POST','get'])
-def list_orders(request):
-    userID = request.Form['userID']
-    user = User.query.get_or_404(userID)
-    return jsonify({'orders': [Order.to_json() for ]})
--- app/models.py
+++ app/models.py
@@ -124,6 +124,10 @@ class User(UserMixin, db.Model):
             return None
         return User.query.get(data['id'])
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class Good(db.Model):
     __tablename__ = 'goods'
