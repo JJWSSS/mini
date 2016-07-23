a56d534683904e0617a50f1684c3780a7b409659
unknown <>
2016-07-23 16:08:31 +0800

    order

--- app/models.py
+++ app/models.py
@@ -19,16 +19,16 @@ class Order(db.Model):
     count = db.Column(db.Integer, nullable=False)
     status = db.Column(db.Integer, nullable=False)
 
-    def get_seller_orders(self,sellerID):
+    def get_seller_ordersID(self,sellerID):
         orders = db.session.query(Order).filter(sellerID == Order.sellerID).all()
         return orders
 
-    def get_buyer_orders(self,buyerID):
+    def get_buyer_ordersID(self,buyerID):
         orders = db.session.query(Order).filter(buyerID == Order.sellerID).all()
         return orders
 
     def get_order_detail(self,orderID):
-        orders = db.session.query(Order).filter(orderID == Order.orderID).one()
+        orders = db.session.query(Order).filter(orderID == Order.orderID).first()
         return orders
 
     def to_json(self):
