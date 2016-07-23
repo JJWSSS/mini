84ec415cf7c9f054a16ca781cfc1f02d2bde2d23
unknown <>
2016-07-23 18:34:34 +0800

    order

--- app/api/order.py
+++ app/api/order.py
@@ -12,39 +12,97 @@ from flask_login import current_user,login_required
 @login_required
 @api.route('/list_seller_orders', method = ['POST'])
 def list_seller_orders():
-    requestinfo = request.json
-    start = requestinfo.start
-    stop = start + requestinfo.count - 1
+    start = request.form['start']
+    stop = start + request.form['count'] - 1
     sellerID = current_user.userID
     if not sellerID:
         return jsonify(
             {
-                'status' : 'Fail',
-                'message' : 'User Not Login'
+                'status' : 0,
+                'message' : 'Fail: User Not Login'
             }
         )
+
     ordersID = Order.query(Order.orderID).filter(sellerID == Order.sellerID).slice(start,stop)
+    if not ordersID:
+        return jsonify(
+            {
+                'status' : 2,
+                'message' : 'Fail: No order'
+            }
+        )
+
     orderlist = list()
     for orderID in ordersID:
         orderinfo = Order.query(Order).filter(orderID == Order.orderID).first()
         orderinfo = dict(orderinfo,**Good.query(Good.goodName).filter(Good.goodID == orderinfo.goodID).first())
         orderinfo = dict(orderinfo,**User.query(User.userName).filter(User.userID == orderinfo.buyerID).first())
         orderlist.append(orderinfo)
-    return jsonify()
+
+    return jsonify(
+        {
+            'status' : 1,
+            'message' : 'Success',
+            'data' : orderlist
+        }
+    )
 
 
 # 列出作为买家的订单
+# params[POST]:
+#   'start' [int]
+#   'count' [int]
 @api.route('/list_seller_orders', method = ['POST'])
 def list_buyer_orders():
+    start = request.form['start']
+    stop = start + request.form['count'] - 1
     buyerID = current_user.userID
-    orders = Order.get_buyer_orders(buyerID)
-    return orders
+    if not buyerID:
+        return jsonify(
+            {
+                'status' : 'Fail',
+                'message' : 'User Not Login'
+            }
+        )
+
+    ordersID = Order.query(Order.orderID).filter(buyerID == Order.buyerID).slice(start,stop)
+    if not ordersID:
+        return jsonify(
+            {
+                'status' : 2,
+                'message' : 'Fail: No order'
+            }
+        )
+
+    orderlist = list()
+    for orderID in ordersID:
+        orderinfo = Order.query(Order).filter(orderID == Order.orderID).first()
+        orderinfo = dict(orderinfo,**Good.query(Good.goodName).filter(Good.goodID == orderinfo.goodID).first())
+        orderinfo = dict(orderinfo,**User.query(User.userName).filter(User.userID == orderinfo.sellerID).first())
+        orderlist.append(orderinfo)
+    return jsonify(
+        {
+            'status' : 'Success',
+            'message' : orderlist
+        }
+    )
 
 # 获取订单详情
+@login_required
 @api.route('/get_order_detail', method = ['POST'])
-def get_order_detail()
-    requeseInfo = request.json
-    orderID = requeseInfo['orderID']
-    order = Order.get_order_detail(orderID)
-    return order
+def get_order_detail():
+    orderID = request.form['orderID']
+    orderDetail = Order.query(Order).filter(Order.orderID == orderID).first()
+    return jsonify(
+        {
+            'status' : 1,
+            'message' : 'Success',
+            'data' : orderDetail
+        }
+    )
+
+@login_required
+@api.route('/create_order',method = ['POST'])
+def create_order():
+    orderinfo = request.json
 
