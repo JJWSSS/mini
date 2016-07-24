94cfc58dd7fb1c722e1d8d2ce1fe6552e8f211b3
lee <lika52090@gmail.com>
2016-07-24 23:50:12 +0800

    Commproxy.query以下的函数都还没注释

--- app/api/comment.py
+++ app/api/comment.py
@@ -12,6 +12,10 @@ from app.api.user import user_info
 
 
 def appendUserInfo(getJson):
+    """
+    用于在获取评论列表的返回数据中附上用户信息的装饰器，
+    装饰CommentProxy.query方法用
+    """
     @wraps(getJson)
     def __doAppendImage(self, args):
         ret = getJson(self, args)
@@ -31,6 +35,10 @@ def appendUserInfo(getJson):
 
 
 def model2Json(getAll):
+    """
+    用于装饰CommentProxy.query方法用的装饰器，
+    将查询结果从模型对象转换为Json格式的数据
+    """
     @wraps(getAll)
     def __doToJson(self, args):
         ret = getAll(self, args)
@@ -41,6 +49,9 @@ def model2Json(getAll):
 
 
 def getAll(qurey):
+    """
+    用于装饰CommentProxy.query的装饰器，取出查询对象中的所有结果
+    """
     @wraps(qurey)
     def __doGetAll(self, args):
         return qurey(self, args).all()
@@ -49,6 +60,10 @@ def getAll(qurey):
 
 
 def limitAndStartAddtion(qurey):
+    """
+    用于装饰CommentProxy.query的装饰器，可以增加对'limit'和'start'
+    参数的支持
+    """
     @wraps(qurey)
     def __doLimitAndStart(self, args):
         ret = qurey(self, args)
@@ -66,7 +81,21 @@ def limitAndStartAddtion(qurey):
 
 
 class CommentProxy:
+    """
+    Comment模型的代理类，该类负责与模型进行交互。
+    该类中使用到的大部分关于模型的信息都是从配置
+    中动态获得，故而与模型是一种及其松散的耦合关系
+    Note:
+        现在该类显然太大了，职责不单纯，除了负责操作模型
+        之外，还有一些'工具方法'，这些与操作模型无关的方法
+        可以提炼成为一个单独的helper类。
+    """
     def __init__(self, fileds):
+        """
+        构造函数
+        :param fileds: [list]
+            该参数储存了所有模型的域(数据库表的列)名称。
+        """
         self._filerDict = {}
         self._fileds = fileds
         self.__Comment = Comment
@@ -87,17 +116,32 @@ class CommentProxy:
         self.__Comment = _comment
 
     def toJson(self, model):
+        """
+        将模型对象转换成为字典对象
+        :param model  [Comment]:
+            需要被转换的模型对象
+        :return  [dict]:
+            转换后的字典对象，该字典的key是模型对象的属性名,
+            value 是模型对象的属性值
+        """
         jsonDict = {}
         for filed in self._fileds:
             jsonDict[filed] = model.__getattribute__(filed)
         return jsonDict
 
-    @staticmethod
-    def filterArgs(args, filerFiled):
+    def filterArgs(self, args):
+        """
+        参数过滤器，用于过滤前端输入的非法参数
+        :param args [dict]:
+            所有请求参数的字典
+        :return: [dict]
+            返回的合法参数字典，字典的key是合法参数的名字，
+            value是合法参数的值
+        """
         ret = {}
         if args is not None:
             for key, value in args.items():
-                if key in filerFiled:
+                if key in self._fileds:
                     ret[key] = value
         return ret
 
@@ -110,10 +154,16 @@ class CommentProxy:
     @getAll
     @limitAndStartAddtion
     def query(self, args=None):
+        """
+        查询函数，通过输入的查询条件查询符合条件的模型
+        :param args [dict]:
+            
+        :return:
+        """
         if not args:
             return self.Comment.query.filter_by(status=0)
 
-        self._filerDict = self.filterArgs(args, self._fileds)
+        self._filerDict = self.filterArgs(args)
         self._filerDict['status'] = 0  # 这里假设了表的结构，依赖于表的结构，需要重构
         ret = self.Comment.query.filter_by(**self._filerDict)
         return ret
@@ -123,7 +173,7 @@ class CommentProxy:
         if isVailed and not isVailed(args):
             return self.makeRetJson(0, 'invailed arguments')
 
-        args = self.filterArgs(args, self._fileds)
+        args = self.filterArgs(args)
         db.session.add(self.Comment(**args))
         exp = None
         try:
@@ -138,7 +188,7 @@ class CommentProxy:
         return self.makeRetJson(1, message)
 
     def delete(self, args):
-        self._filerDict = self.filterArgs(args, self._fileds)
+        self._filerDict = self.filterArgs(args)
         if len(self._filerDict) <= 0:
             return self.makeRetJson(0, '0 comments have been deleted')
         self._filerDict['status'] = 0
