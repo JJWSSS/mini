7efa1473fd4f2f69b92b87d49573626ddcfae08b
JJW <1012109715@qq.com>
2016-07-26 21:16:45 +0800

    强无敌

--- app/api/good.py
+++ app/api/good.py
@@ -93,9 +93,9 @@ def search():
 def new_good():
     try:
         objects = request.json
-        if objects['price'] < 0:
+        if int(objects['price']) < 0:
             return jsonify({'status': -3, 'data': ['价格为负']})
-        if objects['freeCount'] < 0:
+        if int(objects['freeCount']) < 0:
             return jsonify({'status': -4, 'data': ['剩余数量为负']})
         good = Good(goodName=objects['goodName'], description=objects['description'],
                     freeCount=objects['freeCount'], type=objects['type'],
@@ -145,7 +145,7 @@ def edit_good():
         good = Good.query.get(objects['good_id'])
         if not good:
             return jsonify({'status': -1, 'data': ['商品没有查到']})
-        if objects['price'] < 0:
+        if int(objects['price']) < 0:
             return jsonify({'status': -3, 'data': ['价格为负']})
         good.description = objects['description']
         good.goodName = objects['goodName']
